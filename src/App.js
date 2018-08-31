9e63ee14e37f85d8506377f8eaf02b9191117049
Aditya Dixit <adityadixit06@gmail.com>
2018-08-31 20:22:47 +0530

    Form being generated dynamically by specifying external config file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DynamicFormFactory from "./DynamicFormFactory";
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      config: [],
+      url: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.sendConfig = this.sendConfig.bind(this);
+  }
+
+
+  onSubmit = (config) => {
+    let data = [];
+    if (config.id) {
+      data = this.state.data.filter((d) => {
+        return d.id != config.id
+      });
+    } else {
+      config.id = +new Date();
+      data = this.state.data.slice();
+    }
+    
+    this.setState({
+      data: [config, ...data]
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ url : e.target.value})
+  }
+
+  sendConfig() {
+    fetch(this.state.url)
+        .then(response => response.json())
+    .then( config => this.setState({ config: config } ))
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/users')
+      .then(response => response.json())
+      .then(all => this.setState({ data: all } ))
+
+
+   fetch('http://localhost:5000/trainmodel')
+      .then(response => response.json())
+      .then( config => this.setState({ config: config } ))
+  }
+
+
   render() {
     let club_props = { club: ""};
     let player_props = { player: ""};
@@ -12,10 +63,24 @@ class App extends Component {
       <div className="App">
         <Link to='/clubregistration'><button>Club Registration</button></Link>
         <Link to='/playerdetails'><button>Player Details</button></Link>
+        <input type="text" value={this.state.url} placeholder="Please enter the url for config file" onChange={this.handleChange} />
+        <h3>{this.state.url}</h3>
+        <button onClick={this.sendConfig}>Send Config </button>
+
 
         <Switch>
-        <Route path="/clubregistration" render={ (club_props) => <DynamicFormFactory {...club_props} />} />
-        <Route path="/playerdetails" render={ (player_props) => <DynamicFormFactory {...player_props} />} />
+        <Route path="/clubregistration" render={ (club_props) => 
+                                              <DynamicFormFactory 
+                                                  {...club_props} 
+                                                  config={this.state.config }
+                                                  onSubmit={(config) => this.onSubmit(config)}
+                                                  title="Club Registration"/>} />
+        <Route path="/playerdetails" render={ (player_props) =>
+                                              <DynamicFormFactory 
+                                                  {...player_props}
+                                                  config={this.state.config }
+                                                  onSubmit={(config) => this.onSubmit(config)}
+                                                  title="Player Details" />} />
         </Switch>
       </div>
     );
--- src/DynamicFormFactory.js
+++ src/DynamicFormFactory.js
@@ -2,9 +2,55 @@ import React from "react";
 
 
 class DynamicFormFactory extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	formElements =() => {
+		let config = this.props.config;
+
+		let mapElements = config.map((c) => {
+		let props = c.props || {};
+		let target = c.key;
+
+		let input = <input {...props}
+                    	name={c.name}
+                    	key={c.key}
+                    	value={c.value}
+                     	type={c.type}
+                    	onChange={(e)=>{this.onChange(e, target)}}
+                	/>;
+        return(
+        	<div>
+        		<label key={"label" + c.key}
+        			   htmlFor={c.key}>
+        			   {c.label}
+        		</label>
+        		{input}
+        	</div>);
+		})
+
+		return mapElements;
+	}
+
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		if (this.props.onSubmit) this.props.onSubmit(this.state);
+	}
 
 	render() {
+		let title = this.props.title || "Generated Form";
 		return(
-			<div>Form Factory</div>)
+			<div>
+				<h2>{title}</h2>
+				<form onSubmit={ (e) => { this.onSubmit(e) } } >
+				 {this.formElements()}
+				<button type="submit"> Submit Form</button>
+				</form>
+			</div>)
 	}
 }
+
+export default DynamicFormFactory;
