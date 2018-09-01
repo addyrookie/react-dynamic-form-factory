c6932a1a134bc6fed19150b2cafe1afd83672756
Aditya Dixit <adityadixit06@gmail.com>
2018-09-01 11:57:57 +0530

    Created redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from './reducers';
 
-ReactDOM.render(<BrowserRouter>
-					<App />
-				</BrowserRouter>,
-				 document.getElementById('root'));
+const store = createStore(
+  				rootReducer,
+  				applyMiddleware(thunk)
+);
+
+
+ReactDOM.render(
+	 	<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>,
+		document.getElementById('root'));
 registerServiceWorker();
